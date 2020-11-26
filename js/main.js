6e83b331d0f00b3a0ba575d34de062a77f18ff5b
laminegaci <mohamed61lamine@gmail.com>
2020-11-26 13:06:46 +0100

    List Rendering

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 var app = new Vue({
     el: '#app',
-    data: {
+    data: { 
         product: 'T-shirt',
         image: 'images/vmSocks-green-onWhite.jpg',
         inStock: true,
-        inventory: 10
+        inventory: 10,
+        details: ["80% cotton", "20% polyster", "Gender-neutral"],
+        variants: [
+            {
+                'varintId': 1,
+                'variantColor': "green"
+            },
+            {
+                'varintId': 2,
+                'variantColor': "blue"
+            }
+        ],
+        sizes: ['S','M','L','XL','XXL']
     }
 })
